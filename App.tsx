878bd60988b73f53257a538ca14aaaecce4b6347
Mark Faust <markfaust123@gmail.com>
2024-01-18 20:34:41 -0500

    added basic local notification scheduling and listening in App

--- App.tsx
+++ App.tsx
@@ -1,11 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { LogBox } from "react-native";
+LogBox.ignoreLogs(["new NativeEventEmitter"]); // Ignore log notification by message
+// LogBox.ignoreAllLogs(); //Ignore all log notifications
+import { StatusBar } from "expo-status-bar";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
+import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true,
+    };
+  },
+});
 
 export default function App() {
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log("NOTIFICATION RECEIVED");
+        console.log(notification);
+        const username = notification.request.content.data.userName;
+        console.log(username);
+      }
+    );
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  const permissionsHandler = async () => {
+    const settings = await Notifications.getPermissionsAsync();
+
+    const isGranted = settings.granted;
+    if (isGranted) {
+      Alert.alert(
+        "Permission has already been granted!",
+        "You can receive notifications"
+      );
+    } else {
+      const request = await Notifications.requestPermissionsAsync();
+
+      if (request.granted) {
+        Alert.alert(
+          "You have granted permissions",
+          "You can now receive notifications"
+        );
+      } else {
+        Alert.alert(
+          "You did not grant permissions",
+          "You will be unable to receive notifications"
+        );
+      }
+    }
+  };
+  const scheduleNotificationHandler = () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Notification Title",
+        body: "Body of notification; the details",
+        data: {
+          userName: "UserNameHere",
+        },
+      },
+      trigger: {
+        seconds: 5,
+      },
+    });
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={{ marginBottom: 20 }}>
+        <Button title="Permissions" onPress={permissionsHandler} />
+      </View>
+      <Button
+        title="Schedule Notification"
+        onPress={scheduleNotificationHandler}
+      />
     </View>
   );
 }
@@ -13,8 +87,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
